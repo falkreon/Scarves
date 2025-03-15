4a17da285697792fa96c2301e6420adcafa90401
Falkreon <falkreon@gmail.com>
2025-03-15 03:54:12 -0500

    Fix fatal crash in stapler equipment mixin

--- src/main/java/blue/endless/scarves/ScarvesItems.java
+++ src/main/java/blue/endless/scarves/ScarvesItems.java
@@ -1,6 +1,9 @@
 package blue.endless.scarves;
 
+import net.minecraft.block.Block;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Equipment;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -17,12 +20,24 @@ public class ScarvesItems {
 	public static void register() {
 		SCARF = register( new ScarfItem(), ScarfItem.ID );
 		
-		SCARF_STAPLER = register(new BlockItem(ScarvesBlocks.SCARF_STAPLER, new Item.Settings().rarity(Rarity.UNCOMMON)) , ScarfStaplerBlock.ID);
+		SCARF_STAPLER = register(new HeadEquippableBlock(ScarvesBlocks.SCARF_STAPLER, new Item.Settings().rarity(Rarity.UNCOMMON)) , ScarfStaplerBlock.ID);
 		SCARF_TABLE = register(new BlockItem(ScarvesBlocks.SCARF_TABLE, new Item.Settings().rarity(Rarity.UNCOMMON)), ScarfTableBlock.ID);
 	}
 	
 	private  static <T extends Item> T register(T item, String id) {
 		Registry.register(Registries.ITEM, Identifier.of(ScarvesMod.MODID, id), item);
 		return item;
+	}
+	
+	private static class HeadEquippableBlock extends BlockItem implements Equipment {
+
+		public HeadEquippableBlock(Block block, Settings settings) {
+			super(block, settings);
+		}
+
+		@Override
+		public EquipmentSlot getSlotType() {
+			return EquipmentSlot.HEAD;
+		}
 	}
 }
